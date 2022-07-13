86ff7d00512de96b6a249fb5727a73001b3dfad6
ibsonngit <thiaremohamed.mt@gmail.com>
2022-07-13 09:01:31 +0000

    Creation du projet teste Liquibase

---
+++ src/main/java/sn/thiare/testeliquibase/TesteLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package sn.thiare.testeliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteLiquibaseApplication.class, args);
+    }
+
+}
